005fd0288e7fddfb0df0bd46e74e7c4da29cd56a
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2025-02-15 18:38:32 +0100

    Day 3 Bonus - Rust

---
+++ src/day3_bonus.rs
@@ -0,0 +1,36 @@
+mod utils;
+
+fn main() {
+    let lines = utils::read_input();
+
+    let mut result = 1;
+    for diff in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)].iter() {
+        let x_diff = diff.0;
+        let y_diff = diff.1;
+        result *= trees_on_slope(&lines, x_diff, y_diff);
+    }
+
+    println!("{}", result);
+}
+
+fn trees_on_slope(lines: &Vec<String>, x_diff: usize, y_diff: usize) -> usize {
+    let mut result = 0;
+    let mut x = 0;
+
+    for y in (0..lines.len()).step_by(y_diff) {
+        let line = &lines[y];
+
+        if line.as_bytes()[x] == b'#' {
+            result += 1;
+        }
+
+        x = (x + x_diff) % line.len();
+    }
+
+    result
+}
+
+//     result = 1
+//     for xDiff, yDiff in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+//         result *= trees_on_slope(lines, xDiff=xDiff, yDiff=yDiff)
+//     print(result)
