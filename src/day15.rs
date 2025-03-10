d0292e5cc346f188c0a2a221c1d7b98fe121a071
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2025-03-10 18:26:29 +0100

    Day 15

---
+++ src/day15.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+mod utils;
+
+const FINAL_NUMBER: usize = 2020;
+
+fn main() {
+    let lines = utils::read_input();
+    let numbers = parse_input(&lines);
+
+    let last_spoken = get_last_spoken_number(&numbers);
+
+    println!("{}", last_spoken);
+}
+
+fn parse_input(lines: &Vec<String>) -> Vec<i32> {
+    lines
+        .iter()
+        .flat_map(|line| line.split(','))
+        .map(|x| x.parse().unwrap())
+        .collect()
+}
+
+fn get_last_spoken_number(numbers: &Vec<i32>) -> i32 {
+    let mut spoken_at: HashMap<i32, usize> = HashMap::new();
+
+    // Initialize with input numbers
+    &numbers[0..numbers.len() - 1]
+        .iter()
+        .enumerate()
+        .for_each(|(i, number)| {
+            spoken_at.insert(*number, i);
+        });
+
+    // Iterate to the result
+    let mut last_spoken = *numbers.last().unwrap();
+    (numbers.len() - 1..FINAL_NUMBER - 1).for_each(|i| {
+        let next_spoken = match spoken_at.get(&last_spoken) {
+            Some(last_spoken_at) => i - last_spoken_at,
+            None => 0,
+        };
+
+        spoken_at.insert(last_spoken, i);
+        last_spoken = next_spoken as i32;
+    });
+
+    last_spoken
+}
